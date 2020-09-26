6c3eb48de4c2904f15b6f741f74ec69bc473ba1c
lvdong <2789501979@qq.com>
2020-09-26 16:22:15 +0800

    吕东

---
+++ demo/src/main/java/com/kgc/service/ClassesService.java
@@ -0,0 +1,13 @@
+package com.kgc.service;
+
+import com.kgc.pojo.Classes;
+
+import java.util.List;
+
+/**
+ * @author shkstart
+ * @create 2020-09-26 15:45
+ */
+public interface ClassesService {
+    List<Classes> selectAll();
+}
